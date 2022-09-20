10fa89675f9907d086a31bca77d780da9282a55a
Toni <89582846+Noccis@users.noreply.github.com>
2022-09-20 10:45:30 +0200

    view binding implemented on main and home fragment

--- app/src/main/java/com/example/memoandjokesapp/HomeFragment.kt
+++ app/src/main/java/com/example/memoandjokesapp/HomeFragment.kt
@@ -7,27 +7,39 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.memoandjokesapp.databinding.FragmentHomefragmentBinding
 
 class HomeFragment : Fragment() {
 
+    private var _binding: FragmentHomefragmentBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_homefragment, container, false)
-        val goToJokesButton = view.findViewById<Button>(R.id.btn_jokes)
+        _binding = FragmentHomefragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val goToJokesButton = binding.btnJokes
         goToJokesButton.setOnClickListener {
             findNavController().navigate(R.id.action_homeragment_to_jokesFragment)
         }
 
-        val goToMemosButton = view.findViewById<Button>(R.id.btn_memos)
+        val goToMemosButton = binding.btnMemos
         goToMemosButton.setOnClickListener {
             findNavController().navigate(R.id.action_homeragment_to_memoFragment)
         }
 
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
